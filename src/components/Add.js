e579d8e1e46e633eebb2835454ebabc715b11dbe
Erika Lopes <201810099@acad.unijuazeiro.edu.br>
2021-07-20 20:57:25 -0300

    add input search

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import {useState} from 'react'
 
 export const Add = () => {
+    const [query, setQuery] = useState('')
+
+    const onChange = event => {
+        event.preventDefault()
+
+        setQuery(event.target.value)
+    }
+
     return (
         <div className="add-page">
-            <h1>Add page</h1>
+            <div className="container">
+                <div className="add-content">
+                    <div className="input-wrapper">
+                        <input type="text" placeholder="Procure por um filme"
+                            value={query}
+                            onChange={onChange}/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Watched.js
+++ src/components/Watched.js
@@ -1,4 +1,3 @@
-import React from 'react'
 
 export const Watched = () => {
     return (
